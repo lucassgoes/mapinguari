f77908c0d852d42c0438ded02f3e41c9c578fd78
Lucas Goes <44140476+lucassgoes@users.noreply.github.com>
2026-03-10 23:06:21 -0300

    Multiplayer básico finalizado, agora começando o relay
    Finalizei o básico do multiplayer, agora tenho que fazer funcionar o relay, baixei os assets só por enquanto.

--- mapinguari/Assets/ClientPlayerMove.cs
+++ mapinguari/Assets/ClientPlayerMove.cs
@@ -8,7 +8,10 @@ public class ClientPlayerMove : NetworkBehaviour
     [SerializeField] private PlayerInput m_PlayerInput;
     [SerializeField] private StarterAssetsInputs m_StarterAssetsInputs;
     [SerializeField] private FirstPersonController m_FirstPersonController;
-    [SerializeField] private BasicRigidBodyPush m_BasicRigidBodyPush;
+    [SerializeField] private BasicRigidBodyPush m_BasicRigidBodyPush; 
+    [SerializeField] private GameObject m_MainCamera;
+    [SerializeField] private GameObject m_PlayerFollowCamera;
+    [SerializeField] private GameObject m_PlayerCameraRoot;  
 
     void Awake()
     {
@@ -16,6 +19,9 @@ public class ClientPlayerMove : NetworkBehaviour
         m_StarterAssetsInputs.enabled = false;
         m_FirstPersonController.enabled = false;
         m_BasicRigidBodyPush.enabled = false;
+        m_MainCamera.SetActive(false);
+        m_PlayerFollowCamera.SetActive(false);
+        m_PlayerCameraRoot.SetActive(false);
     }
 
    public override void OnNetworkSpawn()
@@ -28,6 +34,9 @@ public class ClientPlayerMove : NetworkBehaviour
             m_StarterAssetsInputs.enabled = true;
             m_FirstPersonController.enabled = true;
             m_BasicRigidBodyPush.enabled = true;
+            m_MainCamera.SetActive(true);
+            m_PlayerFollowCamera.SetActive(true);
+            m_PlayerCameraRoot.SetActive(true);
         }
     }
 }
