6b68d69b582fad0601648a8cde2d3df81b27873b
Lucas Goes <44140476+lucassgoes@users.noreply.github.com>
2026-03-11 21:18:47 -0300

    Adicionei o console e configurei o relay

---
+++ mapinguari/Assets/Scripts/RelayManager.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using IngameDebugConsole;
+using System.Threading.Tasks;
+using Unity.Networking.Transport.Relay;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+
+public class RelayManager : MonoBehaviour
+{
+    public async void Start()
+    {
+        DebugLogConsole.AddCommand( "CreateRelay", "Creates the relay", CreateRelay );
+        DebugLogConsole.AddCommand<string>( "JoinRelay", "Joins the relay", JoinRelay );
+
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+          Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);  
+        };
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    public async Task<string> CreateRelay()
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(3);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+            Debug.Log(joinCode);
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetHostRelayData(
+                allocation.RelayServer.IpV4,
+                (ushort)allocation.RelayServer.Port,
+                allocation.AllocationIdBytes,
+                allocation.Key,
+                allocation.ConnectionData
+            );
+
+            NetworkManager.Singleton.StartHost();
+
+            return joinCode;
+
+        } catch(RelayServiceException e)
+        {
+            Debug.Log(e);
+
+            return null;
+        }
+        
+    }
+
+    public async void JoinRelay(string joinCode)
+    {
+        try
+        {
+           Debug.Log("Joining Relay with " + joinCode);
+           JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode); 
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetClientRelayData(
+                joinAllocation.RelayServer.IpV4,
+                (ushort)joinAllocation.RelayServer.Port,
+                joinAllocation.AllocationIdBytes,
+                joinAllocation.Key,
+                joinAllocation.ConnectionData,
+                joinAllocation.HostConnectionData
+            );
+
+            NetworkManager.Singleton.StartClient();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+        
+    }
+}
